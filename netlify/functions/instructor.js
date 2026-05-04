a6dfef0e2c5ac8fccc93abd20e5f6c70cdc80b29
Tauro3575 <waltertordecilla75@gmail.com>
2026-05-04 10:03:18 +0200

    Update instructor.js

--- netlify/functions/instructor.js
+++ netlify/functions/instructor.js
@@ -1,22 +1,33 @@
 exports.handler = async (event) => {
   const APPS_SCRIPT_URL = process.env.GOOGLE_SHEETS_URL;
-  
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
 
   try {
-    const response = await fetch(APPS_SCRIPT_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: event.body
-    });
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify(data)
-    };
+    if (event.httpMethod === 'GET') {
+      const params = new URLSearchParams(event.queryStringParameters || {});
+      const url = `${APPS_SCRIPT_URL}?${params.toString()}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return {
+        statusCode: 200,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify(data)
+      };
+    }
+
+    if (event.httpMethod === 'POST') {
+      const response = await fetch(APPS_SCRIPT_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: event.body
+      });
+      const data = await response.json();
+      return {
+        statusCode: 200,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify(data)
+      };
+    }
+
   } catch(e) {
     return {
       statusCode: 500,
